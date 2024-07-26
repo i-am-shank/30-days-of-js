e27608c8dbd5f3032376214b07002695fb644a9b
i-am-shank <shashanks.ug18.ph@nitp.ac.in>
2024-07-26 17:59:42 +0530

    experimenting with modules

---
+++ random/src/index.js
@@ -0,0 +1,8 @@
+import { logarithm } from "./mathOperations";
+
+const helloYoinky = () => {
+    console.log("Hello Behenu!");
+    console.log("log of 1000 base 10 = ", logarithm(1000, 10));
+};
+
+helloYoinky();
