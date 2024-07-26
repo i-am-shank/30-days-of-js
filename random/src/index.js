47fd3a390bee4dbd58f545b5a29529dd2825fa17
i-am-shank <shashanks.ug18.ph@nitp.ac.in>
2024-07-26 20:02:00 +0530

    revert module experiments

--- random/src/index.js
+++
@@ -1,8 +0,0 @@
-import { logarithm } from "./mathOperations";
-
-const helloYoinky = () => {
-    console.log("Hello Behenu!");
-    console.log("log of 1000 base 10 = ", logarithm(1000, 10));
-};
-
-helloYoinky();
--- random/src/mathOperations.js
+++
@@ -1,6 +0,0 @@
-import { log } from "mathjs";
-
-export const logarithm = (num, base) => {
-    const ans = log(num, base);
-    return ans;
-};
